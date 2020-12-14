bf57ff8a81cdc9f70025f8c1e6fda38b51eeb489
aws-thitlwin <aws-thitlwin@example.com>
2020-12-14 11:33:31 +0630

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FlutterFire Codelab: Friendly Eats
+A cross-platform Flutter port of the "Friendly Eats" Firestore codelab.
+
+This is the source code for the FlutterFire Friendly Eats codelab. It includes 
+start and end versions of the required code.
+
+## Ref Link
+https://codelabs.developers.google.com/codelabs/friendlyeats-flutter?hl=en&continue=https%3A%2F%2Fcodelabs.developers.google.com%2F#9
---
+++ lib/src/model/data.dart
@@ -0,0 +1,71 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import './filter.dart';
+import './restaurant.dart';
+import './review.dart';
+
+// This is the file that Codelab users will primarily work on.
+Future<void> addRestaurant(Restaurant restaurant) {
+  final restaurants = FirebaseFirestore.instance.collection('restaurants');
+  return restaurants.add({
+    'avgRating': restaurant.avgRating,
+    'category': restaurant.category,
+    'city': restaurant.city,
+    'name': restaurant.name,
+    'numRatings': restaurant.numRatings,
+    'photo': restaurant.photo,
+    'price': restaurant.price,
+  });
+}
+
+Stream<QuerySnapshot> loadAllRestaurants() {
+  return FirebaseFirestore.instance
+      .collection('restaurants')
+      .orderBy('avgRating', descending: true)
+      .limit(50)
+      .snapshots();
+}
+
+List<Restaurant> getRestaurantsFromQuery(QuerySnapshot snapshot) {
+  return snapshot.docs.map((DocumentSnapshot doc) {
+    return Restaurant.fromSnapshot(doc);
+  }).toList();
+}
+
+Future<Restaurant> getRestaurant(String restaurantId) {
+  return FirebaseFirestore.instance
+      .collection('restaurants')
+      .doc(restaurantId)
+      .get()
+      .then((DocumentSnapshot doc) => Restaurant.fromSnapshot(doc));
+}
+
+Future<void> addReview({String restaurantId, Review review}) {
+  // TODO: Complete the "Write data in a transaction" step.
+  return Future.value();
+}
+
+Stream<QuerySnapshot> loadFilteredRestaurants(Filter filter) {
+  // TODO: Complete the "Sorting and filtering data" step.
+  return Stream<QuerySnapshot>.value(null);
+}
+
+void addRestaurantsBatch(List<Restaurant> restaurants) {
+  restaurants.forEach((Restaurant restaurant) {
+    addRestaurant(restaurant);
+  });
+}
